35a4424dca952e4e2ea5d314f63c8cd97cc538b0
Stephen Zhou <stv1024@gmail.com>
2019-06-04 20:22:48 +0800

    upgrade sdk

--- dcl/src/game.ts
+++ dcl/src/game.ts
@@ -83,6 +83,13 @@ function start() {
             }
         }
     }
+    
+    // Create screenspace component
+    const canvas = new UICanvas()
+
+    // Create a textShape component, setting the canvas as parent
+    const text = new UIText(canvas)
+    text.value = 'Hello world!'
 
     const input = Input.instance;
 
