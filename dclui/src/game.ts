bfa441cb45b8e8511576c31f43ed10f43d570297
Stephen Zhou <stv1024@gmail.com>
2019-06-12 23:30:05 +0800

    test ui

--- dclui/src/game.ts
+++ dclui/src/game.ts
@@ -1,58 +1,66 @@
+
+class SpriteInfo {
+  sourceLeft = 0;
+  sourceTop = 0;
+  sourceWidth = 32;
+  sourceHeight = 32;
+  constructor(left: number, top: number, width: number, height: number) {
+    this.sourceLeft = left;
+    this.sourceTop = top;
+    this.sourceWidth = width;
+    this.sourceHeight = height;
+  }
+}
+
+function setImageSprite(image: UIImage, spriteInfo: SpriteInfo) {
+  image.sourceLeft = spriteInfo.sourceLeft;
+  image.sourceTop = spriteInfo.sourceTop;
+  image.sourceWidth = spriteInfo.sourceWidth;
+  image.sourceHeight = spriteInfo.sourceHeight;
+}
 {
-    // Create screenspace component
-    const canvas = new UICanvas()
+  // Create screenspace component
+  const canvas = new UICanvas()
 
-    const text = new UIText(canvas)
-    text.value = 'Hello world!'
+  const text = new UIText(canvas)
+  text.value = 'Hello world!'
 
-    // const rect = new UIContainerRect(canvas)
-    // rect.width = 500
-    // rect.height = '80%'
-    // rect.color = Color4.Blue()
-    // rect.opacity = 0.5
+  // const rect = new UIContainerRect(canvas)
+  // rect.width = 500
+  // rect.height = '80%'
+  // rect.color = Color4.Blue()
+  // rect.opacity = 0.5
 
-    const pnlBottom = new UIContainerRect(canvas);
-    pnlBottom.hAlign = 'center';
-    pnlBottom.vAlign = 'bottom';
-    pnlBottom.color = new Color4(1,1,1,0.05);
-    pnlBottom.opacity = 1;
-    pnlBottom.positionY = 155;
-    pnlBottom.height = 50;
-    pnlBottom.width = 400;
-    
-    {
-      const txt0 = new UIText(pnlBottom)
-      txt0.value = '####●';
-      txt0.color = Color4.Black();
-      txt0.fontSize = 15
-      txt0.width = 120
-      txt0.height = 20
-      txt0.vAlign = 'center'
-      txt0.positionX = -30;
-      txt0.positionY = 50;
+  const pnlBottom = new UIContainerRect(canvas);
+  pnlBottom.hAlign = 'center';
+  pnlBottom.vAlign = 'bottom';
+  pnlBottom.color = new Color4(1, 1, 1, 0.05);
+  pnlBottom.opacity = 1;
+  pnlBottom.positionY = 155;
+  pnlBottom.height = 50;
+  pnlBottom.width = 400;
 
-      let imageTexture = new Texture("images/placeholder1.png");
-      const icon = new UIImage(canvas, imageTexture);
-      icon.sourceLeft = 0;
-      icon.sourceTop = 0;
-      icon.sourceWidth = 32;
-      icon.sourceHeight = 32;
-      icon.width = 16;
-      icon.height = 16;
-    }
 
-    //left
-    const pnlLeft = new UIContainerRect(canvas);
-    pnlLeft.hAlign = 'left';
-    pnlLeft.vAlign = 'center';
-    pnlLeft.color = new Color4(1,1,0,0.2);
-    pnlLeft.positionY = 0;
-    pnlLeft.height = 400;
-    pnlLeft.width = 150;
+  //lefttop
+  const pnlLeft = new UIContainerRect(canvas);
+  pnlLeft.hAlign = 'left';
+  pnlLeft.vAlign = 'top';
+  pnlLeft.color = new Color4(1, 1, 0, 0.2);
+  pnlLeft.positionY = 32;
+  pnlLeft.height = 150;
+  pnlLeft.width = 200;
+  {
+    let imageTexture = new Texture("images/placeholder1.png");
+    let spriteInfo = new SpriteInfo(0,0,32,32);
+    const icon = new UIImage(pnlLeft, imageTexture);
+    setImageSprite(icon, spriteInfo);
+    icon.width = 16;
+    icon.height = 16;
+  }
 }
 
 class USys {
-  update(){
+  update() {
     log(Camera.instance.rotation.eulerAngles);
   }
 }
